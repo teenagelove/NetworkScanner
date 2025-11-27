81514494a33acc78f759d85b64c5fc1e25f4e965
Danil Kazakov <dani4upa586@gmail.com>
2025-11-27 11:53:06 +0900

    feat: initialize NetworkScanner SwiftUI project

---
+++ NetworkScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkScanner
+//
+//  Created by Danil Kazakov on 2025/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
