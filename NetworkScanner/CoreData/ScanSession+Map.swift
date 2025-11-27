49c818ae333bcb508b1a66facf7c809e7b222c7b
Danil Kazakov <dani4upa586@gmail.com>
2025-11-27 20:46:26 +0900

    feat(coredata): Implement CoreData model and mapping

---
+++ NetworkScanner/CoreData/ScanSession+Map.swift
@@ -0,0 +1,22 @@
+//
+//  ScanSession+Map.swift
+//  NetworkScanner
+//
+//  Created by Danil Kazakov on 2025/11/27.
+//
+
+import CoreData
+import Foundation
+
+extension ScanSession {
+    func mapToModel() -> ScanSessionModel {
+        let deviceEntities = self.devices?.allObjects as? [Device] ?? []
+        let deviceModels = deviceEntities.map { $0.mapToScannedDevice() }
+        
+        return ScanSessionModel(
+            id: self.id ?? UUID(),
+            date: self.date ?? Date(),
+            devices: deviceModels.sorted(by: { $0.discoveryDate > $1.discoveryDate })
+        )
+    }
+}
